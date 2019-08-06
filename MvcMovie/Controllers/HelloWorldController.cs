555a3d426f0855a55f8c1f2d4af404f661f7adc1
erniker <josepablomedinagrande@gmail.com>
2019-08-06 20:04:11 +0200

    Introducción básica a las vistas

--- MvcMovie/Controllers/HelloWorldController.cs
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -4,15 +4,18 @@ using System.Text.Encodings.Web;
 namespace MvcMovie.Controllers
 {
     public class HelloWorldController : Controller
-    { 
-        // 
-        // GET: /HelloWorld/
-        public string Index()
+    {
+        /// <summary>
+        /// Método que devuelve un objeto view()
+        /// http://localhost:xxxx/HelloWorld
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Index()
         {
-            return "Esta es mi acción por defecto...";
+            return View();
         }
-        // 
-        // GET: /HelloWorld/Welcome/ 
+
+
         /// <summary>
         /// Método de ejemplo para usarlo en un endpoint con paso de parámetros
         /// http://localhost:xxxx/HelloWorld/Welcome?name=Rick&numtimes=4
